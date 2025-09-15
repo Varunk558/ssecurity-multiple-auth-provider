4b1a12e287e5a486206ce2d7bfffd71deed2df30
Varun <varunkothapally18@gmail.com>
2025-09-15 14:02:19 -0500

    Added DemoController and main class

---
+++ src/main/java/com/ssmap/ssecurity_multiple_auth_provider/SsecurityMultipleAuthProviderApplication.java
@@ -0,0 +1,13 @@
+package com.ssmap.ssecurity_multiple_auth_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsecurityMultipleAuthProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsecurityMultipleAuthProviderApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ssmap/ssecurity_multiple_auth_provider/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.ssmap.ssecurity_multiple_auth_provider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo(){
+        return "demo!!";
+    }
+}
