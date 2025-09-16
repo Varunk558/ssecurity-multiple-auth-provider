22e48308d3ff768ae49785fdf0de9d849e556727
Varun <varunkothapally18@gmail.com>
2025-09-16 10:51:18 -0500

    Created Custom Authentication ApiKeyAuthentication

---
+++ src/main/java/com/ssmap/ssecurity_multiple_auth_provider/config/authentication/ApiKeyAuthentication.java
@@ -0,0 +1,57 @@
+package com.ssmap.ssecurity_multiple_auth_provider.config.authentication;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ApiKeyAuthentication implements Authentication {
+
+    @Getter
+    private final String key;
+
+    private boolean authenticated;
+
+    @Override
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean authenticated) throws IllegalArgumentException {
+        this.authenticated = authenticated;
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+
+
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
