95fe7ee00590a2e8e2b1b8931b45d67c1c323b6e
Varun <varunkothapally18@gmail.com>
2025-09-16 10:48:49 -0500

    Created Custom Authentication Provider ApiKeyProvider

---
+++ src/main/java/com/ssmap/ssecurity_multiple_auth_provider/provider/ApiKeyProvider.java
@@ -0,0 +1,30 @@
+package com.ssmap.ssecurity_multiple_auth_provider.provider;
+
+import com.ssmap.ssecurity_multiple_auth_provider.config.authentication.ApiKeyAuthentication;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+
+@AllArgsConstructor
+public class ApiKeyProvider implements AuthenticationProvider {
+
+    private final String key;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        ApiKeyAuthentication auth =(ApiKeyAuthentication) authentication;
+        if(key.equals(auth.getKey())){
+            auth.setAuthenticated(true);
+            return auth;
+        }
+        throw new BadCredentialsException("Bad credentials");
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return ApiKeyAuthentication.class.equals(authentication);
+    }
+}
